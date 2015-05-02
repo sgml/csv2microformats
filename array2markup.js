1703853d4301d364e2a3b672e1ccefdc2f76eb93
sgml <paul.sweatte@etrade.com>
2015-05-02 09:33:27 -0700

    Fixed indexing bug
    Added check for index mismatch

--- array2markup.js
+++ array2markup.js
@@ -1,26 +1,30 @@
-var markup = function(index, value)
+function markup(value, index)
+{
+var open = {
+0: "<time class=|published| datetime=|value|>value",
+1: "<h1 class=|entry-title|>value",
+2: "<p class=|entry-summary|>value",
+3: "<article class=|hentry|>",
+4: "<a class=|bookmark| href=|value| rel=|bookmark|>"
+};
+
+var close = {
+0: "</time>",
+1: "</h1>",
+2: "</p>",
+3: "</article>",
+4: "</a>"
+};
+
+if (index)
   {
-  var open = {
-  0: "<time class=|published| datetime=|value|>value",
-  1: "<h1 class=|entry-title|><a class=|bookmark| href=|http://www.allmovie.com/movie/value:/| rel=|bookmark|>value",
-  2: "<p class=|entry-summary|>value",
-  3: "<article class=|hentry|"
-  };
-  
-  var close = {
-  0: "</time>",
-  1: "</a> </h1>",
-  2: "</p>",
-  3: "</article>"
-  };
-  
-  return String().concat(open[index].replace("|","\u0022","g").replace("value",value,"g") , close[index]); 
+  return open[index].replace("|","\u0022","g").replace("value",value,"g") + close[index];
   }
-  
-var transform = function(value, index, source){return markup(index,value)}
+else
+  {
+  return "";
+  }
+};
 
-var foo = "2014, Beyond the Lights, Gugu Mbatha-Raw, Gina Prince-Bythewood, http://www.allmovie.com/movie/beyond-the-lights-v592866".split(",");
-
-var bar = foo.map(transform)
-
-
+var foo = "2014, Beyond the Lights, Gugu Mbatha-Raw, Gina Prince-Bythewood, http://www.allmovie.com/movie/beyond-the-lights-v592866".split(", ");
+var bar = foo.map(markup)
